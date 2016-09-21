277e2507e47937aae0a0cff35fef0969912bb02c
leo adonia <adonia@adonia.local>
2016-09-22 00:53:23 +0800

    Update: add bootstrap form in app.ts

--- angular2/reddit/day3/app.ts
+++ angular2/reddit/day3/app.ts
@@ -4,15 +4,23 @@ import {Component} from "angular2/core";
 @Component({
     selector: 'reddit',
     template: `
-    <div>Hello {{name}}</div>
+    <form role="form">
+        <div class="form-group">
+            <h3>Add a link</h3>
+        </div>
+        <div class="form-group">
+            <label for="titleInput">Title</label>
+            <input type="text" class="form-control" id="titleInput" placeholder="title of article">
+        </div>
+        <div class="form-group">
+            <label for="linkInput">Link</label>
+            <input type="text" class="form-control" id="linkInput" placeholder="link of article">
+        </div>
+        <button class="btn btn-info">Submit</button>
+    </form>
     `
 })
-class Reddit {
-    name: string;
+class RedditApp {}
 
-    constructor() {
-        this.name = 'reddit';
-    }
-}
-bootstrap(Reddit);
+bootstrap(RedditApp);
 
